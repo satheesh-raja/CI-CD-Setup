8025d4fa0b21627d14a34d45f94400e34708fa92
CORP\z038839 <satheesh.raja@rntbci.com>
2024-05-06 10:57:24 +0530

    Build hook

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,25 @@ plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
+
+tasks.register("copyGitHooks", Copy::class.java) {
+    description = "Copies the git hooks from /git-hooks to the .git folder."
+    group = "git hooks"
+    from("$rootDir/scripts/pre-commit")
+    into("$rootDir/.git/hooks/")
+}
+tasks.register("installGitHooks", Exec::class.java) {
+    description = "Installs the pre-commit git hooks from /git-hooks."
+    group = "git hooks"
+    workingDir = rootDir
+    commandLine = listOf("chmod")
+    args("-R", "+x", ".git/hooks/")
+    dependsOn("copyGitHooks")
+    doLast {
+        logger.info("Git hook installed successfully.")
+    }
+}
+
+afterEvaluate {
+    tasks.getByPath(":app:preBuild").dependsOn(":installGitHooks")
+}
